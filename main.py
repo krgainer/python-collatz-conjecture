925f46c67b8d6f543e00b3ada7bd518e2895eb93
krgainer <86863603+krgainer@users.noreply.github.com>
2022-04-10 00:38:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import math
 fourTwoOneCount = 0
 fourTwoOneCountMax = 3
 inf = math.inf
-y = 1.02
+## This value can be any floating point number, ideally between 0 and 2. 
+y = 1.000000000000007
 
 def main():
 	v = 1
+	# This checks to make sure that v times your multiplier (y) is within range of inifinty
 	while v <= (inf - (v*y)):
 		v *= y
 		collatz(v)
@@ -17,7 +19,10 @@ def collatz(inputVal):
 	fourTwoOneCount = 0
 	totalCount = 0
 	while fuckVal:
+		## This is the collatz conjecture "game logic"
 		inputVal = inputVal / 2 if inputVal % 2 == 0 else inputVal * 3 + 1
+		## Allows x amount of bounces of [4, 2, 1, 4], determined by fourTwoOneCountMax, to be played before moving to the next number.
+		## this part is written weird, but only for furture data gathering purposes		
 		if inputVal == 4 and fourTwoOneCount <= fourTwoOneCountMax:
 			fourTwoOneCount += 1
 			print (fourTwoOneCount)
@@ -26,6 +31,7 @@ def collatz(inputVal):
 		else:
 			totalCount += 1
 
+# This outputs the number of times the collatz conjecture was run before hitting a [4, 2, 1, 4] loop. 
 	print (f"Fuck caught at: {totalCount}")
 
 main()
