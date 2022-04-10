e26561906bb55ebffe612e50523a5fcc946fc6f1
krgainer <86863603+krgainer@users.noreply.github.com>
2022-04-10 00:29:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 
 fourTwoOneCount = 0
+fourTwoOneCountMax = 3
 inf = math.inf
 y = 1.02
 
@@ -11,18 +12,20 @@ def main():
 		collatz(v)
 
 def collatz(inputVal):
-	print(f"Fuck: {inputVal}")
+	print(f"Current Input: {inputVal}")
 	fuckVal = True
 	fourTwoOneCount = 0
 	totalCount = 0
 	while fuckVal:
 		inputVal = inputVal / 2 if inputVal % 2 == 0 else inputVal * 3 + 1
-		if inputVal == 4 and fourTwoOneCount <= 3:
+		if inputVal == 4 and fourTwoOneCount <= fourTwoOneCountMax:
 			fourTwoOneCount += 1
-			if fourTwoOneCount == 4:
-				fuckVal = False
 			print (fourTwoOneCount)
-		totalCount += 1
-	print (totalCount)
+		if fourTwoOneCount == fourTwoOneCountMax+1:
+			fuckVal = False
+		else:
+			totalCount += 1
+
+	print (f"Fuck caught at: {totalCount}")
 
 main()
