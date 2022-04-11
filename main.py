d82fa06d58f367b56846924b38da1b72433b9ede
Kieran Gainer <86863603+krgainer@users.noreply.github.com>
2022-04-11 13:09:02 -0400

    just using a breaking for loop now

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import math
+import random
 
-## The amount of times the 4 - 2 - 1 loop is allowed to repeat before moving to the next number.
+# The amount of times the 4 - 2 - 1 loop is allowed to repeat before moving to the next number.
 fourTwoOneCountMax = 3
-## This value can be any floating point number, ideally between 0 and 2. MUST BE A FLOAT
-y = 1.
+# This value can be any floating point number, ideally between 0 and 2. MUST BE A FLOAT
+y = 1.0000006
 # validateded through 1.3222491729511548e+16
 z = 0
 
@@ -15,25 +16,27 @@ def main():
 		v *= y
 		if v != z:
 			collatz(v)
+		else:
+			print ("The multiplier entered has resulted in an infinite loop.")
+			break
 
 def collatz(inputVal):
 	initialValue = inputVal
-	GameRunning = True
 	fourTwoOneCount = 0
 	totalCount = 0
-	while GameRunning:
-		## This is the collatz conjecture "game logic".bool()
-		## If inputVal is an even number, divide it by 2, otherwise multiply it by 3 and add one. 
+	while True:
+		# This is the collatz conjecture "game logic"
+		# If inputVal is an even number, divide it by 2, otherwise multiply it by 3 and add one. 
 		inputVal = inputVal / 2 if inputVal % 2 == 0 else inputVal * 3 + 1
-		## Allows x amount of bounces of [4, 2, 1, 4], determined by fourTwoOneCountMax, to be played before moving to the next number.
-		## this part is written weird, but only for furture data gathering purposes		
-		if inputVal == 4 and fourTwoOneCount <= fourTwoOneCountMax:
+		# Allows x amount of bounces of [4, 2, 1, 4], determined by fourTwoOneCountMax, to be played before moving to the next number.
+		if inputVal == 4:
 			fourTwoOneCount += 1
-		if fourTwoOneCount == fourTwoOneCountMax+1:
-			GameRunning = False
+			if fourTwoOneCount == fourTwoOneCountMax:
+				# Loop breaks once the amount of bounces of [4, 2, 1, 4] is reached.
+				break
 		else:
 			totalCount += 1
-# This outputs the number of times the collatz conjecture was run before hitting a [4, 2, 1, 4] loop. 
+	# This outputs the number of times the collatz conjecture was run before hitting a [4, 2, 1, 4]loop. 
 	print (f"The number '{round(initialValue, 6)}' returned 4,2,1 in {totalCount} iterations.")
 
 main()
