825fcbdc7b002e588f55a48ae195eab8694a0b7d
Kieran Gainer <86863603+krgainer@users.noreply.github.com>
2022-04-12 22:50:15 -0400

    added a code playground, maximum altitude, and a refined way to check for floats and ints,

---
+++ code playground.py
@@ -0,0 +1,29 @@
+import itertools
+import time
+
+
+def itertoolsFunction():
+	for i in itertools.count(1):
+		print (i)
+		if i < 10000000000:
+			break
+
+def plusOneFunction():
+	e = 0
+	while True:
+		e += 1
+		print (e)
+		if e < 10000000000:
+			break
+
+## White a function that inciments by 1 but reallu fast.
+
+def main():
+	start_time = time.time()
+	itertoolsFunction()
+	print(f"--- Itertools comepleted in: {time.time() - start_time} seconds! ---")
+	start_time = time.time()
+	plusOneFunction()
+	print(f"--- i+=1 comepleted in: {time.time() - start_time} seconds! ---")
+
+main()
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import math
-import random
 
-# The amount of times the 4 - 2 - 1 loop is allowed to repeat before moving to the next number.
-fourTwoOneCountMax = 3
-# This value can be any floating point number, ideally between 0 and 2. MUST BE A FLOAT
-y = 1.006
+
+fourTwoOneCountMax = 3 # The amount of times the 4 - 2 - 1 loop is allowed to repeat before moving to the next number.
 z = 0
 
-def main():
+
+def floatNum():
+	y = 1.00006 # This value can be any floating point number, ideally between 0 and 2. MUST BE A FLOAT
 	x = 1
 	# This checks to make sure that v times your multiplier (y) is within range of inifinty
 	while x <= ( (math.inf) - (x*y) ):
@@ -19,15 +18,31 @@ def main():
 			print ("The multiplier entered has resulted in an infinite loop.")
 			break
 
+
+def intNum():
+	x = 0
+	# This checks to make sure that v times your multiplier (y) is within range of inifinty
+	while True:
+		x += 1
+		collatz(x)
+
+
 def collatz(n):
 	initialValue = n
 	totalCount = 0
-	while n != 1:
+	nMax = 0
+	while n != 1 or 0:
 		# This is the collatz conjecture "game logic"
 		# If n is an even number, divide it by 2, otherwise multiply it by 3 and add one. 
 		n = n / 2 if n % 2 == 0 else n * 3 + 1
 		totalCount += 1
-	# This outputs the number of times the collatz conjecture was run before hitting a [4, 2, 1, 4]loop. 
-	print (f"The number '{round(initialValue, 6)}' returned 4,2,1 in {totalCount} iterations.")
+		nMax = n if n > nMax else nMax
+	# This outputs the number of times the collatz conjecture was run before hitting a [4, 2, 1, 4] loop. 
+	print (f"The number '{round(initialValue, 6)}' returned 4,2,1 in {totalCount} iterations. The highest number reached was {nMax}.")
+
+
+def main():
+	intNum()
+
 
 main()
